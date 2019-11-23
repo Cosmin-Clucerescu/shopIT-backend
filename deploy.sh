ebb276c106901c55d2c8f77fb9ab85148f611833
Cosmin Clucerescu <marius.clucerescu@orange.com>
2019-11-23 15:52:25 +0200

    Disable csrf middleware

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
-    #!/bin/bash
-    git clone https://github.com/alexlucaci/shopIT-backend
-    cd shopIT-backend
-    git pull
-    if [ -d "venv" ]
-    then
-        echo "venv exists"
-    else
-        python -m virtualenv venv
-    fi
-    source venv/bin/activate
-    python3 pip install -r requirements.txt
-    killall python3
-    python3 manage.py migrate
-    python3 manage.py runserver 0.0.0.0:8080
+#!/bin/bash
+git clone https://github.com/alexlucaci/shopIT-backend
+cd shopIT-backend
+git pull
+if [ -d "venv" ]
+then
+    echo "venv exists"
+else
+    python -m virtualenv venv
+fi
+source venv/bin/activate
+python3 pip install -r requirements.txt
+killall python3
+python3 manage.py migrate
+python3 manage.py runserver 0.0.0.0:8080
